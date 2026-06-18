c4e9450fa7ec8c3ae34730434cd9cd17ed4ab9df
aannss7790-stack <aannss7790@gmail.com>
2026-06-18 06:17:02 +0300

    index.js

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const qrcode = require('qrcode-terminal');
 const client = new Client({
     puppeteer: {
         executablePath: '/usr/bin/chromium',
-        args: ['--no-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     },
     authStrategy: new LocalAuth()
 });
 
 client.on('qr', (qr) => {
-    // هذا التعديل يجعل الباركود صغيراً جداً
+    // هذا التنسيق هو الأفضل للـ Logs في Render لمنع التشوه
+    console.log('انسخ الكود التالي واستخدم أي موقع لتحويله إلى باركود:');
+    console.log(qr); 
+    
+    // أو استخدم هذا لعرضه بشكل صغير جداً
     qrcode.generate(qr, { small: true });
 });
 
