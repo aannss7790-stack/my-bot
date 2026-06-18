669f1d10004d0142d1e59476391e97665537b9a7
aannss7790-stack <aannss7790@gmail.com>
2026-06-18 05:13:44 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    puppeteer: {
+        executablePath: '/usr/bin/chromium',
+        args: ['--no-sandbox']
+    },
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('تم الربط بنجاح!');
+});
+
+client.initialize();
