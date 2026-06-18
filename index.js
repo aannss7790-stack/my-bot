fbd3c669bbe72432046e5604bf3b5631e2381486
aannss7790-stack <aannss7790@gmail.com>
2026-06-18 05:39:30 +0300

    index.js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-FROM node:20-bookworm-slim
-RUN apt-get update && apt-get install -y \
-    chromium \
-    fonts-liberation \
-    libnss3 \
-    libxss1 \
-    libasound2 \
-    libatk-bridge2.0-0 \
-    libgtk-3-0 \
-    --no-install-recommends && \
-    rm -rf /var/lib/apt/lists/*
-WORKDIR /app
-COPY package*.json ./
-RUN npm install
-COPY . .
-ENV PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
-# هذا السطر يمنع Render من التوقف بانتظار منفذ
-CMD ["node", "index.js"]
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    puppeteer: {
+        executablePath: '/usr/bin/chromium',
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    },
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('تم الربط بنجاح!');
+});
+
+client.initialize();
